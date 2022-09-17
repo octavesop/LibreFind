90dcd25f6ee381453f08a77e8de4c33382e1a863
정민아 <octavesop@gmail.com>
2022-09-18 00:00:53 +0900

    :adhesive_bandage: app controller의 get hello api를 ping으로 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({ description: 'health check' })
+  @Get('/ping')
+  ping(): string {
+    return this.appService.pong();
   }
 }
