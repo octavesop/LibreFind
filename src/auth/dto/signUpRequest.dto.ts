51c3641357e7a03a8669a018eee8f8b57e965cfd
정민아 <octavesop@gmail.com>
2022-09-18 00:02:08 +0900

    :bug: class-validator 설정에 맞춰 signUp에 설정 추가

--- src/auth/dto/signUpRequest.dto.ts
+++ src/auth/dto/signUpRequest.dto.ts
@@ -1,20 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
 
 // TODO
 // class-validator 달기
 export class SignUpRequest {
   @ApiProperty()
+  @IsString()
   readonly userId: string;
+
   @ApiProperty()
+  @IsString()
   userPw: string;
+
   @ApiProperty()
+  @IsString()
   readonly userName: string;
+
   @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
   readonly userNickname?: string;
+
   @ApiProperty()
+  @IsString()
+  @IsEmail()
   readonly userEmail: string;
+
   @ApiProperty()
+  @IsBoolean()
   readonly agreeEssentialTerm: boolean;
+
   @ApiProperty()
+  @IsBoolean()
   readonly agreeMarketingSend: boolean;
 }
