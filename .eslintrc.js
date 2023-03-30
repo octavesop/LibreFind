2b170a3ce21ef9e2aca3287f12adb6c0c4e4c137
octavesop <octavesop@gmail.com>
2023-03-30 23:41:32 +0900

    :technologist: eslint 작업

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -17,9 +16,26 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'warn', // console.log의 사용을 금함
+    'no-throw-literal': 'warn', // throw "e" => throw new Error("e")
+    'no-underscore-dangle': 'off', // 변수 및 메소드 이름에 언더바 사용을 금함
+    '@typescript-eslint/no-var-requires': 'error', // var 사용을 금함
+    '@typescript-eslint/no-empty-function': 'warn', // 비어있는 함수를 금함
+    '@typescript-eslint/no-unused-vars': 'error', // 선언만 해둔 변수 사용을 금함
+    '@typescript-eslint/no-explicit-any': 'warn', // any 사용을 금함
+    '@typescript-eslint/naming-convention': [
+      // 클래스의 이름은 대문자로 강제함.
+      'error',
+      {
+        selector: 'class',
+        format: ['PascalCase'],
+        custom: { regex: '^I[A-Z]', match: false },
+      },
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: { regex: '^I[A-Z]', match: false },
+      },
+    ],
   },
 };
