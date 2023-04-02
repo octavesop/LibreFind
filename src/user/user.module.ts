dd64138ccc65e4d75db46e0435c35b623c14d01c
octavesop <octavesop@gmail.com>
2023-04-02 21:54:02 +0900

    :adhesive_bandage: user module과 friend module 분리

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Friend } from '../friend/entities/friend.entity';
 import { S3ImageUploadHelper } from '../loaders/s3ImageUploader.helper';
 import { UserController } from './controllers/user.controller';
-import { Friend } from './entities/friend.entity';
 import { User } from './entities/user.entity';
 import { UserService } from './services/user.service';
 
