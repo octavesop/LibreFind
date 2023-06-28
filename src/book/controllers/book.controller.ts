1ffa23f0ff4c823914af28bb72f03276c53e446f
octavesop <octavesop@gmail.com>
2023-06-28 21:46:26 +0900

    :coffin: 사용하지 않는 api 제거

--- src/book/controllers/book.controller.ts
+++ src/book/controllers/book.controller.ts
@@ -1,18 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Controller, Delete, Get, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Payload } from '../../auth/dto/payload.dto';
 import { UserPayload } from '../../decorators/userPayload.decorator';
-import { BookRequest } from '../dto/bookRequest.dto';
 import { FetchBookListResponse } from '../dto/fetchBookListResponse.dto';
 import { BookService } from '../services/book.service';
 
@@ -28,24 +17,6 @@ export class BookController {
     @Query('searchKeyword') searchKeyword: string,
   ): Promise<FetchBookListResponse> {
     return await this.bookService.fetchBookListBySearchKeyword(searchKeyword);
-  }
-
-  @Post('/')
-  async addBook(
-    @Body() request: BookRequest,
-    @UserPayload() userInfo: Payload,
-  ): Promise<void> {
-    return await this.bookService.addBookReview(request, userInfo.userUid);
-  }
-
-  @Post('/review')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async addBookReview(
-    @Body() request: BookRequest,
-    @UserPayload() userInfo: Payload,
-  ): Promise<void> {
-    await this.bookService.addBookReview(request, userInfo.userUid);
-    return;
   }
 
   @Delete('/:userMappingBooksUid')
