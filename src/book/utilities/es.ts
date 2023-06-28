176724a3d5e0eba8fbf7ac7a9a2a642fa6cc6575
octavesop <octavesop@gmail.com>
2023-06-28 21:46:58 +0900

    :necktie: 리뷰 추가 시 그에 맞춰 ES의 책 내역에 해당 평가를 반영하는 로직 추가

--- src/book/utilities/es.ts
+++ src/book/utilities/es.ts
@@ -1,6 +1,5 @@
 import { Logger } from '@nestjs/common';
 import axios from 'axios';
-import { BookRequest } from '../dto/bookRequest.dto';
 
 export const fetchBookInfo = async () => {
   const logger = new Logger(fetchBookInfo.name);
@@ -16,13 +15,65 @@ export const fetchBookInfo = async () => {
     throw new Error(error);
   }
 };
-export const addBookInfo = async (request: BookRequest) => {
-  const logger = new Logger(addBookInfo.name);
+
+export const updateEsBook = async (
+  bookUid: string,
+  review: { emotion: number; genre: number; rate: number },
+) => {
+  const logger = new Logger(updateEsBook.name);
   try {
-    await axios.put(process.env.ES_URL + '/_doc' + '/247365887', request);
-    return;
+    const foundBook: {
+      id: string;
+      title: string;
+      author: string;
+      publisher: string;
+      imageUrl: string;
+      isbn: string;
+      review: {
+        emotion: Record<string, number>;
+        genre: Record<string, number>;
+        rate: number;
+        rateCount: number;
+      };
+    } = (await axios.get(process.env.ES_URL + '/_doc/' + bookUid)).data._source;
+
+    console.log(
+      JSON.stringify({
+        ...foundBook,
+        review: {
+          emotion: {
+            ...foundBook.review?.emotion,
+            [review.emotion]:
+              foundBook.review?.emotion[review.emotion] ?? 0 + 1,
+          },
+          genre: {
+            ...foundBook.review?.genre,
+            [review.genre]: foundBook.review?.genre[review.genre] ?? 0 + 1,
+          },
+          rate: foundBook.review?.rate ?? 0 + review.rate,
+          rateCount: foundBook.review?.rateCount ?? 0 + 1,
+        },
+      }),
+    );
+
+    const { data } = await axios.put(process.env.ES_URL + '/_doc/' + bookUid, {
+      ...foundBook,
+      review: {
+        emotion: {
+          ...foundBook.review?.emotion,
+          [review.emotion]: foundBook.review?.emotion[review.emotion] ?? 0 + 1,
+        },
+        genre: {
+          ...foundBook.review?.genre,
+          [review.genre]: foundBook.review?.genre[review.genre] ?? 0 + 1,
+        },
+        rate: foundBook.review?.rate ?? 0 + review.rate,
+        rateCount: foundBook.review?.rateCount ?? 0 + 1,
+      },
+    });
+    return data;
   } catch (error) {
     logger.error(error);
-    throw new Error(error);
+    throw error(error);
   }
 };
